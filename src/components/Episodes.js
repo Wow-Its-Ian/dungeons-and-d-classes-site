db02a377f3c2a72bece62dbe07de3d27d3c30fcd
Ian Walls <iwalls@brandgage.com>
2022-11-16 17:42:15 -0500

    Added placeholderyoutube playlist

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Episodes() {
   return (
-	<div>Episodes</div>
+    <div style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, margin: 'auto', height: '100%', width: '100%'}}>
+      <div style={{position: 'absolute', left: 0, right: 0, margin: 'auto', top: '60px', fontSize: '20px', fontWeight: 'bold'}}>Coming Soon!  Please enjoy content from our sister channel, Trans_Tales</div>
+      <iframe style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, margin: 'auto'}} width="1020" height="630" src="https://www.youtube.com/embed/videoseries?list=PLBJrwluKneJ90UPNDLKLCLmAxtAhnSl8-" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    </div>
   )
 }
 
