e5715ae06c3a8cc13b301c136aa1dd02e6a742cb
Ian Walls <iwalls@brandgage.com>
2022-11-18 07:19:57 -0500

    Updated video styling

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -4,7 +4,8 @@ function Episodes() {
   return (
     <div style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, margin: 'auto', height: '100%', width: '100%'}}>
       <div style={{position: 'absolute', left: 0, right: 0, margin: 'auto', top: '60px', fontSize: '20px', fontWeight: 'bold'}}>Coming Soon!  Please enjoy content from our sister channel, Trans_Tales</div>
-      <iframe style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, margin: 'auto'}} width="1020" height="630" src="https://www.youtube.com/embed/videoseries?list=PLBJrwluKneJ90UPNDLKLCLmAxtAhnSl8-" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+      <iframe style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, margin: 'auto', height: '315px', width: '560px'}} src="https://www.youtube.com/embed/videoseries?list=PLBJrwluKneJ90UPNDLKLCLmAxtAhnSl8-" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   )
 }
